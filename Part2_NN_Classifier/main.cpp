a23d39fcfa812c43d0f97f6194dc9f914df4dd3f
“CarlosLoeza” <loeza.carlos@yahoo.com>
2021-06-02 00:30:59 -0700

    Create Part2 to classify and validate a new dataset

---
+++ Part2_NN_Classifier/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Part2_NN_Classifier
+//
+//  Created by Carlos Loeza on 6/2/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
